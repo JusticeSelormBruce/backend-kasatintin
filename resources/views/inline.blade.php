ce26fd35738454774789c58519f7ff75510790f1
JusticeSelormBruce <justiceselormbruce@gmail.com>
2020-08-14 16:36:20 -0700

    corrections in inline blade file on image index

--- resources/views/inline.blade.php
+++ resources/views/inline.blade.php
@@ -7,7 +7,8 @@ $inline = $post
 <div class="row">
     @foreach ($inline as $item)
         <div class="col-6">
-            <a href="/post_show/{{ $item->id }}"> <img src="{{ $item->images[0]->url }}" alt="read also"></a>
+            <a href="/post_show/{{ $item->id }}"> <img src="{{ $item->images[0]['url'] }}" height="100" class="w-100" alt="read also"></a>
         </div>
+       
     @endforeach
 </div>
